afe04e8bdbd34cb8775e83e885180d4fe931e7d0
sagdish <forsi1@yahoo.com>
2019-07-03 00:14:23 -0700

    first commit

---
+++ src/components/Coffee.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Coffee extends React.Component {
+  render() {
+    return (
+      <div className="coffeeContainer">
+       <h2>who loves coffee?</h2>
+      </div>
+    );
+  }
+}
+
+export default Coffee;
---
+++ src/components/Library.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Library extends React.Component {
+  render() {
+    return (
+      <div className="coffeeContainer">
+       <h2>Do you like studying at libraries?</h2>
+      </div>
+    );
+  }
+}
+
+export default Library;
