5bb09cc4000a6e4a13f5f474d46fdb1bf67afab4
sagdish <forsi1@yahoo.com>
2019-07-06 02:11:09 -0700

    removed API key and placed it in env variable

--- src/components/coffeeShops/cumaica.js
+++ src/components/coffeeShops/cumaica.js
@@ -1,27 +1,76 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 import '../components.css';
 
-const mapStyles = {
-  width: '35%',
-  height: '300px'
-};
+// const mapStyles = {
+//   width: '100%',
+//   height: '300px'
+// };
 
 class Cumaica extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    }
+  }
+
+  onMarkerClick = (props, marker, event) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+  }
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  }
+
 
   render() {
+    //console.log(process.env.REACT_APP_SFSPOTS_MAPS_API);
     return (
       <div>
         <Map
           google={this.props.google}
           zoom={15}
-          style={mapStyles}
+          // style={mapStyles}
           initialCenter={{
             lat: 37.783138,
             lng: -122.461632
-          }}
-        />
+          }}>
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Cumaica Coffee shop'}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            title={'The marker`s title will appear as a tooltip.'}
+            name={'SOMA'}
+            position={{lat: 37.778519, lng: -122.405640}}
+          />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          >
+            <div>
+              <h3>{this.state.selectedPlace.name}</h3>
+            </div>
+          </InfoWindow>
+
+
+          
+        </Map>
         
       </div>
     )
@@ -29,7 +78,7 @@ class Cumaica extends React.Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmekl7erk7QVTK2Rqbw0EG_7SZ3KdlH7Q'
+  apiKey: process.env.REACT_APP_SFSPOTS_MAPS_API
 })(Cumaica);
 
-//module.export = Cumaica
+//module.exports = Cumaica
