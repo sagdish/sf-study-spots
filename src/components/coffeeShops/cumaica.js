3a1938bd3764ae3893bdc5a0ec81cf3ba24ecb71
sagdish <forsi1@yahoo.com>
2019-07-07 23:59:09 -0700

    played with google maps api, preparing for dinamicly rendering pages with ajax call to api

--- src/components/coffeeShops/cumaica.js
+++ src/components/coffeeShops/cumaica.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import axios from 'axios';
 
 import '../components.css';
 
+
 // const mapStyles = {
-//   width: '100%',
-//   height: '300px'
+//   width: '80%',
+//   height: '250px'
 // };
+const apiKey = process.env.REACT_APP_SFSPOTS_MAPS_API;
+let place;
 
 class Cumaica extends React.Component {
   constructor() {
@@ -17,6 +21,19 @@ class Cumaica extends React.Component {
       selectedPlace: {},
     }
   }
+
+
+  componentDidMount() {
+    //const rocafe = 'Cumaica'
+    axios
+      .get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=cafe%20enchante&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${apiKey}`)
+      .then(response => {
+         place = response.data;
+         console.log(place);
+      })
+      .catch(err => console.log('my error:', err));
+  }
+  
 
   onMarkerClick = (props, marker, event) => {
     this.setState({
@@ -57,6 +74,7 @@ class Cumaica extends React.Component {
             title={'The marker`s title will appear as a tooltip.'}
             name={'SOMA'}
             position={{lat: 37.778519, lng: -122.405640}}
+            
           />
           <InfoWindow
             marker={this.state.activeMarker}
@@ -67,18 +85,12 @@ class Cumaica extends React.Component {
               <h3>{this.state.selectedPlace.name}</h3>
             </div>
           </InfoWindow>
-
-
-          
         </Map>
-        
       </div>
     )
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_SFSPOTS_MAPS_API
+  apiKey
 })(Cumaica);
-
-//module.exports = Cumaica
