03232ac323c2592bc29ad12d27cb9670c30eeb37
sagdish <forsi1@yahoo.com>
2019-07-24 19:07:54 -0700

    Home component working and seting state via hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,27 +4,34 @@ import axios from 'axios';
 function getPlaces() {
   return axios.get(`http://localhost:5000/api/spots`)
     .then(response => {
+      console.log('server response:', response)
       return response;
     })
-    .catch();
+    .catch(err => console.log(err));
 };
 
 export default function Home() {
-  const [ state, setState ] = useState([]);
-
+  const [ spotList, setSpotList ] = useState([]);
+  //console.log(spotList);
   useEffect(() => {
     getPlaces()
-      .then(response => {
-        response.data.map( ({ name, position, favorite, neighborhood, photos, rating }) => {
-          console.log({ name, position, favorite, neighborhood, photos, rating });
+      .then(response => 
+        response.data.map(spots => {
+          //console.log({ name, position, favorite, neighborhood, photos, rating });
+          const { name, position, favorite, neighborhood, photos, rating, _id } = spots;
+          return { name, position, favorite, neighborhood, photos, rating, _id }
         })
-      })
-      .catch();
-  });
+      )
+      .then(spots => setSpotList(spots))
+      .catch()
+  }, []);
 
   return (
     <div>
       <h1>This is home component</h1>
+      {spotList.map(spots =>
+      <div key={spots._id}>{spots.name} {spots.position.lat} {spots.rating} </div>
+      )}
     </div>
   )
   
