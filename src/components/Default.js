f1f73ef6a5d6b94153398a0f78bbb30626a9a10f
sagdish <forsi1@yahoo.com>
2019-08-27 02:18:45 -0700

    add react-reveal dependancyto have sliding effects for intro screen

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
+import Fade from 'react-reveal/Fade'
 
 import './components.css';
+import { hidden } from 'ansi-colors';
 
 
 // This will be greeting component, which will welcome new user 
@@ -10,18 +12,34 @@ import './components.css';
 
 
 class Coffee extends React.Component {
+  state = {
+    showButton: false,
+  }
+
+  
   render() {
+
+    setTimeout(()=> {
+      this.setState({ showButton: true })
+    }, 5000)
+
     return (
       <div className="welcome">
-          <Link to="/home">
-            <Button size="large" style={{
-                backgroundColor: "#8a4ddb",
-                color: "white"
-              }}>
-              Explore My Web App
-            </Button>
-            {/* <button type="primary">who loves coffee?</button> */}
-          </Link>
+        <Fade right>
+          <h1>WELCOME</h1>
+        </Fade>
+        <p>Hi, I decided to create this app as a guide of the study friendly coffee shops in San Francisco. 
+          In my journey learning how to code, one of the big obstacles were finding suitable place to study and focus.
+        </p>
+        <Link to="/home">
+          {this.state.showButton?
+          <Fade top><Button size="large" style={{
+              backgroundColor: "#8a4ddb",
+              color: "white"
+            }}>
+            Explore It
+          </Button></Fade> : <button style={{display: "hidden", marginTop: "22px"}}></button> }
+        </Link>
       </div>
     );
   }
