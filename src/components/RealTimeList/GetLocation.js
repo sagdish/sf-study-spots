0b9fdd855b2005412dde2a70dff8b542d1ff74af
sagdish <forsi1@yahoo.com>
2019-09-02 13:37:16 -0700

    fix disappearing maps container below 500px

--- src/components/RealTimeList/GetLocation.js
+++ src/components/RealTimeList/GetLocation.js
@@ -35,7 +35,7 @@ export default function LocationGetter() {
             const coords = pos.coords;
             console.log('your current location: ', coords);
             setPosition({ lat: coords.latitude, lng: coords.longitude });
-        }, err => console.log(err), {timeout: 5000})
+        }, err => console.error(err)) // add timeout here: {timeout: 5000}
       } else {
         alert("please enable your locaiton");
       }
@@ -71,7 +71,6 @@ export default function LocationGetter() {
     console.log(spotList);
   console.groupEnd();
 
-    
   return (
     <>
       {loading ? <Loader /> : (
