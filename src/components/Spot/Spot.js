a0c27befd8cf7dc7ae2f0f958409339b9776ca24
sagdish <forsi1@yahoo.com>
2020-10-28 22:10:59 -0700

    image fiting in container, display loader while image loads

--- src/components/Spot/Spot.js
+++ src/components/Spot/Spot.js
@@ -6,6 +6,7 @@ import fs from 'fs';
 import '../components.css';
 import './Spot.css';
 import MapsView from '../Maps/MapsView';
+import loading from '../images/Spin.svg';
 
 async function getPhoto(photoreference) {
 
@@ -24,7 +25,7 @@ function Spot(props) {
   //1 set state instead of var
   //2 call to places api to get photo const photo = 'https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyBteViGo-N3uCUN2NWHlAnPg1ow8hOoplk&photoreference=CmRaAAAAatRnbGQcNn2Lu23dL30yedDqUaFslwk0vHeUkB5Uuypn-MsrBFIiyTWYRZpS6eSPXwJ1OaXquTIZ4Wc6_WDiRRw8YUKnxKHt30374R8QLGbrIJUhetIDHOOWncnPyeo3EhDXQpkhWC9y3NNERenNFTMsGhSzAcuWrTo-sqAy7VdD4VSZfsg8Rw&maxheight=500'
   // 3 display photo
-  const [ spot, setSpot ] = useState(null)
+  const [spot, setSpot] = useState(null)
   const [photo, setPhoto] = useState(null)
   useEffect(() => {
     if (props.location.state) {
@@ -32,7 +33,7 @@ function Spot(props) {
       setSpot(props.location.state.spot)
     }
     if (spot && spot.photos && spot.photos.length) {
-      console.log(spot);
+      console.log('photos array', spot.photos);
       getPhoto(spot.photos[0].photo_reference)
         .then(res => {
           setPhoto(res);
@@ -42,16 +43,20 @@ function Spot(props) {
 
   }, [props.location.state, props.location.state.spot, spot]);
   
-  console.log(photo);
+  console.log('photo', photo);
   
   return (
     spot ? (
     <div className="SpotView">
       {/* <div className="CardContainer"> */}
       <div className="InfoContainer">
-      <img alt='' src={photo}/>
-        <p style={{fontStyle: "italic", fontSize: '10px'}}>
-          soon photo of the spot <br/> will appear here</p>
+        <div className="image-container">
+          {photo ? 
+            <img className="image" alt='' src={photo}/> :
+            <img className="" alt='' src={loading}/> 
+          }</div>
+        {/* <p style={{fontStyle: "italic", fontSize: '10px'}}>
+          soon photo of the spot <br/> will appear here</p> */}
         <h2>{spot.name}</h2>
         <p>{spot.neighborhood}</p>
         <p>{spot.type}</p>
